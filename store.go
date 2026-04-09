e895870cb5bc88733dfe1b11b4396c028c135b7b
Nima Naghibi <dude.nimanaghibi@gmail.com>
2026-04-09 17:33:58 +0330

    feat: create a writeStream func to wrtie down a file byte by byte on the disk

--- store.go
+++ store.go
@@ -1,6 +1,10 @@
 package main
 
-import "io"
+import (
+	"io"
+	"log"
+	"os"
+)
 
 type PathTransformFunc func(string) string
 
@@ -23,5 +27,27 @@ func NewStore(opts StoreOpts) *Store {
 }
 
 func (s *Store) writeSteam(key string, r io.Reader) error {
-	pathName := key
+	pathName := s.PathTransformFunc(key)
+
+	if err := os.MkdirAll(pathName, os.ModePerm); err != nil {
+		return err
+	}
+
+	// for now this is fine till i find a way to name the file using a hash func
+	filename := "somefilename"
+
+	f, err := os.Open(pathName + "/" + filename)
+	if err != nil {
+		return err
+	}
+
+	// writing down the file byte by byte on the disk
+	n, err := io.Copy(f, r)
+	if err != nil {
+		return err
+	}
+
+	log.Printf("written (%d) bytes to disk", n)
+
+	return nil
 }
